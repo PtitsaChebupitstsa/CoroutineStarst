cdf7d7370de12a902d60616b87b74dfeb39fcce7
Ptitsa_Chebupitstsa <kerugeru@gmail.com>
2023-12-27 01:21:07 +0600

    14.8 Structured Concurrency

---
+++ app/src/main/java/com/example/coroutinestarsts/MainViewModel.kt
@@ -0,0 +1,43 @@
+package com.example.coroutinestarsts
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.concurrent.thread
+
+class MainViewModel : ViewModel() {
+    private val parentJob = Job()
+    private val coroutineScope = CoroutineScope(Dispatchers.Main + parentJob)
+    fun method() {
+       val childJob1= coroutineScope.launch {
+            delay(3000)
+            Log.d(LOG_TAG, "Первая корутина закончила работу")
+        }
+
+        val childJob2= coroutineScope.launch {
+            delay(2000)
+            Log.d(LOG_TAG, "Вторая корутина закончила работу")
+        }
+        thread {
+            Thread.sleep(1000)
+            parentJob.cancel()
+            Log.d(LOG_TAG,"активна ли родительская Job ${parentJob.isActive}")
+        }
+        Log.d(LOG_TAG,parentJob.children.contains(childJob1).toString())
+        Log.d(LOG_TAG,parentJob.children.contains(childJob2).toString())
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        coroutineScope.cancel()
+    }
+
+    companion object {
+        const val LOG_TAG = "MainViewModel"
+    }
+}
